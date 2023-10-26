1cc5f19f73fb0ee6d8d4722e63d2a088c2fac65a
Orlando <57908549+OrlandoP97@users.noreply.github.com>
2023-10-25 20:31:36 -0400

    refactorize

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-import "./config/mongo.js";
-import express, { Router } from "express";
-import bp from "body-parser";
-import gatewayRouter from "./routes/gateway.js";
+import './config/mongo.js';
+import express from 'express';
+import bp from 'body-parser';
+import gatewayRouter from './routes/gateway.js';
 
 const app = express();
 
 /* Middlewares */
 app.use(bp.json());
-app.use("/gateways", gatewayRouter);
+app.use('/gateways', gatewayRouter);
 
 /* Start server */
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log('Server is running on port 3000');
 });
 
 export default app;
